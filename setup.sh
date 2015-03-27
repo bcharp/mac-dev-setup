326878933526d30d86beae8c576cc69aa3e58674
Boris <boris.charpentier@gmail.com>
2015-03-27 10:44:23 +0100

    add cask

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 ln -s ./.vim/.vimrc ~/.vimrc
 
 # Tools
+brew install git
 brew install ack
 brew install node
 brew install openssl
@@ -18,11 +19,20 @@ brew install task
 npm install -g bower
 npm install -g grunt-cli
 
-# Go and install 
+# For apps
+brew install caskroom/cask/brew-cask
 
+# Apps
 # Chrome : https://www.google.fr/chrome/browser/desktop/#
-# CopyClip : https://itunes.apple.com/us/app/copyclip-clipboard-history/id595191960?mt=12
+brew cask install google-chrome
 # Sublime Text : http://www.sublimetext.com/
+brew cask install sublime-text
 # Rescue time : https://www.rescuetime.com/
+brew cask install rescuetime
 # Charles : http://www.charlesproxy.com/
+brew cask install charles
 # MacDown : http://macdown.uranusjr.com/download/latest/
+brew cask install macdown
+
+# Manual install
+# CopyClip : https://itunes.apple.com/us/app/copyclip-clipboard-history/id595191960?mt=12
